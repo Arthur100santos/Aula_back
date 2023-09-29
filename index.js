631e9e39d85ccbcaaf708361fe00e9a1a49ac759
Arthur100santos <arthur.s4antos543@gmail.com>
2023-09-29 14:43:12 -0300

    Projeto configurado

--- index.js
+++ index.js
@@ -6,4 +6,62 @@ const flash = require("express-flash");
 
 const app = express();
 
-const conn = require('./db/conn');
+const conn = require("./db/conn");
+
+//Import Models
+
+//Import Rotas
+
+//Import Controller
+
+//Configurar engine
+app.engine("handlebars", exphdbs.engine());
+app.set("view engine", "handlebars");
+
+//Configurar JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Middleware para sessões
+app.use(
+  session({
+    name: "session",
+    secret: "nosso_secret", //Quanto maior a cripto melhor
+    resave: false,
+    saveUninitialized: false,
+    store: new FileStore({
+      logFn: function () {},
+      path: require("path").join(require("os").tmpdir(), "sessions"),
+    }),
+    cookie: {
+      secure: false,
+      maxAge: 360000,
+      expires: new Date(Date.now() + 360000),
+      httpOnly: true,
+    },
+  })
+);
+
+//Importar as flash
+app.use(flash());
+
+//Importar os arquivos estáticos
+app.use(express.static("public"));
+
+//Armazenar as sessões nas rotas
+app.use((request, response, next) => {
+  if (request.session.userId) {
+    response.locals.session = request.session;
+  }
+  next();
+});
+
+//Rotas da aplicação
+
+//Conexão e criação das tabelas do banco
+conn
+  .sync()
+  .then(() => {
+    app.listen(3333);
+  })
+  .catch((err) => console.log(err));
